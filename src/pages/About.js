0b02d6f885c6eb3d2fc3ba2d7097736bce842e1d
Harlem <debabel1hackamani@gmail.com>
2023-09-26 13:49:04 +0000

    react world

---
+++ src/pages/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+        exercitationem mollitia blanditiis eaque et reprehenderit, omnis, ad
+        illo error veniam inventore! Veritatis dolorem impedit recusandae harum
+        nesciunt minima eum, quasi sint dicta id earum? Repellendus ullam nemo
+        consectetur quod ex perspiciatis, atque, harum architecto, quos minus
+        nostrum repudiandae consequatur nesciunt. Dolorem sed deserunt deleniti,
+        doloremque eveniet asperiores dolor officiis nulla porro magni, maiores
+        commodi temporibus architecto facilis quisquam quia rem eos? Debitis,
+        soluta! Veritatis mollitia soluta et eos dicta magni molestias,
+        accusamus itaque vel obcaecati sint rerum voluptates placeat illo sequi
+        ullam labore voluptas! Commodi, voluptate! Dolorem accusantium
+        consequuntur odit dignissimos doloremque autem facilis obcaecati dolorum
+        pariatur! Reiciendis incidunt reprehenderit rerum nemo, adipisci
+        consectetur. Possimus odit dolore eaque similique! Assumenda nisi
+        exercitationem voluptate mollitia minima sunt, deleniti ratione vero
+        dolor tempora ea obcaecati facere dolorum eaque expedita enim non
+        aliquam cumque molestiae labore? Voluptas aliquam reprehenderit expedita
+        repellendus cumque maiores eum earum, cupiditate accusamus ab aliquid,
+        aperiam qui praesentium esse voluptatum, obcaecati et atque. Asperiores
+        iste unde iure quaerat earum! Repellat harum rerum ea recusandae, ullam
+        voluptas, sit facilis obcaecati nam similique minima iure quaerat
+        voluptatem quia itaque. Sapiente necessitatibus iure totam deserunt
+        porro reprehenderit maxime nostrum labore accusamus distinctio!
+      </p>
+    </div>
+  );
+};
+
+export default About;
